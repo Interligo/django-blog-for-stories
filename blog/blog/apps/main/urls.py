f427b8d721f2ea3b86e4211bd196a3d515ba59b9
Interligo <interligo@yandex.ru>
2021-02-02 17:36:58 +0300

    Add new app: main page

---
+++ blog/blog/apps/main/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+
+from . import views
+
+
+app_name = 'main'
+urlpatterns = [
+    path('', views.index, name='index'),
+]
+
+# path('<int:story_id>/', views.detail, name='detail'),
+# path('<int:story_id>/leave_comment/', views.leave_comment, name='leave_comment'),
