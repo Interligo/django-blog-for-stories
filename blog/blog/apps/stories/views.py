6bd045b63d3a723cff18260d60a16e7794d098c7
Interligo <interligo@yandex.ru>
2021-02-02 17:39:08 +0300

    Add pagination on comments

--- blog/blog/apps/stories/views.py
+++ blog/blog/apps/stories/views.py
@@ -3,9 +3,11 @@ from django.urls import reverse
 from django.http import Http404
 from django.http import HttpResponseRedirect
 from django.utils.timezone import now
+from django.core.paginator import Paginator
+from django.core.paginator import EmptyPage
+from django.core.paginator import PageNotAnInteger
 
 from .models import Story
-from .models import Comment
 from analytics.models import PageHit
 from analytics.decorators import counted
 
@@ -22,10 +24,23 @@ def detail(request, story_id):
     except:
         raise Http404('Ничего найти не удалось!')
 
-    comments = story.comment_set.order_by('-id')
+    comments = story.comments.order_by('-id')
+    paginator = Paginator(comments, 5)
+    page = request.GET.get('page')
+
+    try:
+        comments_on_page = paginator.page(page)
+    except PageNotAnInteger:
+        comments_on_page = paginator.page(1)
+    except EmptyPage:
+        comments_on_page = paginator.page(paginator.num_pages)
+
     views = PageHit.objects.get(id=story_id)
 
-    return render(request, 'stories/story_detail.html', {'story': story, 'comments': comments, 'views': views})
+    return render(request, 'stories/story_detail.html', {'story': story,
+                                                         'comments': comments,
+                                                         'comments_on_page': comments_on_page,
+                                                         'views': views})
 
 
 # TODO: проверить приходит ли post запрос
@@ -35,8 +50,8 @@ def leave_comment(request, story_id):
     except:
         raise Http404('Ничего найти не удалось!')
 
-    story.comment_set.create(authors_name=request.POST['comment_author_name'],
-                             comment_text=request.POST['comment_text'],
-                             publication_date=now())
+    story.comments.create(authors_name=request.POST['comment_author_name'],
+                          comment_text=request.POST['comment_text'],
+                          publication_date=now())
 
-    return HttpResponseRedirect(reverse('stories:detail', args=(story.id,)))
+    return HttpResponseRedirect(reverse('stories:detail', args=(story.id,)))  # stories:story_detail
