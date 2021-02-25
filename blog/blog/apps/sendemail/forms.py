ab2798e5aa51cf75e8f144b58e12c58795d5445b
Interligo <interligo@yandex.ru>
2021-02-25 17:26:01 +0300

    Update ContactForm: add required fields

--- blog/blog/apps/sendemail/forms.py
+++ blog/blog/apps/sendemail/forms.py
@@ -1,7 +1,25 @@
 from django import forms
+from django.utils.safestring import mark_safe
 
 
 class ContactForm(forms.Form):
-    author = forms.CharField(label='Автор сообщения', required=True)
-    subject = forms.CharField(label='Тема сообщения', required=True)
-    message = forms.CharField(label='Текст сообщения', widget=forms.Textarea, required=True)
+    author = forms.CharField(label=mark_safe('<strong>Автор сообщения</strong>'), widget=forms.TextInput(
+        attrs={
+            'type': 'text',
+            'class': 'form-control'
+        }))
+    subject = forms.CharField(label=mark_safe('<strong>Тема сообщения</strong>'), widget=forms.TextInput(
+        attrs={
+            'type': 'text',
+            'class': 'form-control'
+        }))
+    message = forms.CharField(label=mark_safe('<strong>Текст сообщения</strong>'), widget=forms.Textarea(
+        attrs={
+            'type': 'text',
+            'class': 'form-control'
+        }))
+    email = forms.EmailField(label='Почта для обратной связи', required=False, widget=forms.HiddenInput(
+        attrs={
+            'type': 'email',
+            'class': 'form-control'
+        }))
