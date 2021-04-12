7cefd15064697131f01224ac6270e1d471424dcd
Interligo <interligo@yandex.ru>
2021-04-12 18:26:05 +0300

    Initial commit

---
+++ blog/blog/apps/stories/serializers.py
@@ -0,0 +1,24 @@
+from rest_framework import serializers
+
+from .models import Story
+
+
+class StoryDetailSerializer(serializers.ModelSerializer):
+    """Class for story creating and changing."""
+    class Meta:
+        model = Story
+        fields = '__all__'
+
+
+class StoriesListSerializer(serializers.ModelSerializer):
+    """Class for stories list include only id (for detailing), title and winning place (for Interligo's priding)."""
+    class Meta:
+        model = Story
+        fields = ('id', 'story_title', 'story_award_winning_place')
+
+
+class StoryDetailTextSerializer(serializers.ModelSerializer):
+    """Class for story detailing with text."""
+    class Meta:
+        model = Story
+        fields = ('id', 'story_title', 'story_text')
