1bb0a9d54c355ae6f4aed8b0f7c123fba48fcc85
Interligo <interligo@yandex.ru>
2021-04-12 18:12:36 +0300

    Update urls: add new urlpatterns for API

--- blog/blog/urls.py
+++ blog/blog/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import path
 from django.urls import include
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 urlpatterns = [
@@ -9,5 +11,8 @@ urlpatterns = [
     path('stories/', include('stories.urls')),
     path('book/', include('book.urls')),
     path('grappelli/', include('grappelli.urls')),
+    path('api/v1/auth/', include('rest_framework.urls')),
     path('admin/', admin.site.urls),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
